553b67c5375d4148c0ba2841e84c070dc0c96737
Gill Abada <gill@callifi.com>
2019-02-18 16:30:21 -0500

    add constrcutor to app.js class.

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 import dummyData from './dummy-data.js';
 import Header from './components/Header/Header';
+import PostContainer from './components/PostContainer/PostContainer';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: dummyData
+    }
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div className="App">
       <Header />
+      <PostContainer posts={this.state.posts} />
       </div>
     );
   }
