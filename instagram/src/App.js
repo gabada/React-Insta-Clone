167197259aa554796bad09e4a470ba1f0033a642
Gill Abada <gill@callifi.com>
2019-02-18 15:05:56 -0500

    remove more defaults from CRA.

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import dummyData from './dummy-data.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
       </div>
     );
   }
