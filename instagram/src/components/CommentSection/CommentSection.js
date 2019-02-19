df39fed00f9b36c9b57125134c232cdebc6e7b66
Gill Abada <gill@callifi.com>
2019-02-19 10:19:03 -0500

    change commentSection to a class.

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Comment from '../Comment/Comment';
 import PropTypes from 'prop-types';
 import Dots from '../../img/more-horizontal.svg';
 
 import './CommentSection.css';
 
-const CommentSection = props => {
+class CommentSection extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            comments: props.comments,
+            post: props.post
+        }
+    }
+    render() {
     return (
         <>
-            {props.comments.map((comment, index) => (
+            {this.state.comments.map((comment, index) => (
             <div key={index}>
                 <Comment username={comment.username} text={comment.text} />
             </div>
             ))}
-            <span className="time">{props.timestamp}</span>
+            <span className="time">{this.state.post.timestamp}</span>
             <div>
             <input className="addComment" type="text" placeholder="Add a comment..."/>
             <img src={Dots} alt="more" className="dots"/>
             </div>
         </>
     );
+            }
 };
 
 CommentSection.propTypes = {
