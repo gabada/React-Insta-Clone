6eb382f8ada004b86dde7afd374c42a71a803ae8
Gill Abada <gill@callifi.com>
2019-02-18 19:32:22 -0500

    Finish styling MVP complete.

--- instagram/src/components/Header/Header.js
+++ instagram/src/components/Header/Header.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import Logo from '../../img/insta-header.png';
-import Profile from '../../img/insta-profile.png';
+import Logo from '../../img/instagram.svg';
+import LogoName from '../../img/insta-logo.png';
+import Heart from '../../img/heart.svg';
+import UserLogo from '../../img/user.svg';
+import Compass from '../../img/compass.svg';
 import SearchBar from '../SearchBar/SearchBar';
 
 import './Header.css';
@@ -8,9 +11,16 @@ import './Header.css';
 const Header = props => {
     return (
         <header className="header">
-            <img src={ Logo } alt="instagram"/>
+            <div>
+                <img src={Logo} alt="instagram" className="logoImg"/>{ "|" }
+                <img src={LogoName} alt="instagram" className="logoName"/>
+            </div>
             <SearchBar />
-            <img src={ Profile } alt="profile"/>
+            <div>
+                <img src={ Compass } alt="compass" className="headerCompass"/>
+                <img src={ Heart } alt="heart" className="headerHeart"/>
+                <img src={ UserLogo } alt="profile" className="headerUserLogo"/>
+            </div>
         </header>
     );
 }
