6fdba326eb63e3a89cf5426c319d6be1a5773ede
Gill Abada <gill@callifi.com>
2019-02-20 15:39:59 -0500

    add authentication HOC.

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import PostsPage from './components/PostsPage/PostsPage';
+import withAuthenticate from './components/Authentication/withAuthenticate';
+
+const ComponentFromWithAuthenticate = withAuthenticate(PostsPage);
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-      <PostsPage />
+      <ComponentFromWithAuthenticate />
       </div>
     );
   }
