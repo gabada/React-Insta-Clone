df788127b281992d24629f8822ccd7a314d48ae1
Gill Abada <gill@callifi.com>
2019-02-21 11:13:33 -0500

    fix username margin.

--- instagram/src/styles/Reusables/Username.js
+++ instagram/src/styles/Reusables/Username.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const Username = styled.span`
     margin-left: 10px;
+    margin-bottom: 20px;
     font-weight: ${props => props.weight ? '900' : '700'};
 `;
 
